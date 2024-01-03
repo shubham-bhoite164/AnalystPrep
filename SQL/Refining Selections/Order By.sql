2582ec1ce127d95cf125e51bbfadf2b908dba1f7
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2024-01-03 20:29:35 +0530

    Update Order By.sql

--- SQL/Refining Selections/Order By.sql
+++ SQL/Refining Selections/Order By.sql
@@ -9,3 +9,12 @@ ORDER BY author_lname DESC;
 SELECT * FROM books
 ORDER BY released_year;
 
+SELECT title, author_lname, author_fname, pages
+from books order by pages;  	-- It'll oder by selected columns
+
+-- Order by Multiple Columns
+SELECT title, author_lname, author_fname, pages, released_year
+from books order by author_fname , released_year;
+
+select concat(author_fname, ' ', author_lname) 
+as 'full_name' from books order by author_lname;
