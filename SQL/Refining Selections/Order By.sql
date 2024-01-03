07cd3ae739d015fed67cf5ea248839a87249435f
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2024-01-03 19:54:55 +0530

    Add files via upload

---
+++ SQL/Refining Selections/Order By.sql
@@ -0,0 +1,11 @@
+-- Used to sort the results
+
+SELECT * FROM books
+ORDER BY author_lname; -- Ascending by default
+ 
+SELECT * FROM books
+ORDER BY author_lname DESC;
+ 
+SELECT * FROM books
+ORDER BY released_year;
+
