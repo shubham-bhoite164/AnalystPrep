74f5eb4fe8d11ebb61a6db48bbc2a959430db2f1
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2024-01-03 14:22:41 +0530

    Add files via upload

---
+++ SQL/String Functions/Replace.sql
@@ -0,0 +1,13 @@
+SELECT REPLACE('Hello World', 'Hell', '%$#@');  -- Replace  Hell with %$#@ that is  %$#@o World
+
+SELECT REPLACE('Hello World', 'l', '7'); -- He77o Wor7d
+
+SELECT
+  REPLACE('cheese bread coffee milk', ' ', ' and '); -- cheese and bread and coffee and milk
+  
+SELECT REPLACE(title, ' ', '-') FROM books; -- The-Amazing-Adventures-of-Kavalier-&-Clay
+
+Select replace('Shubham','h','Danger'); -- SDangerubDangeram
+
+
+  
