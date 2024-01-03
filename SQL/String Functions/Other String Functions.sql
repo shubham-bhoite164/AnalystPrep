f0be58ffb3eee157e7bc74211341bb806cda72b7
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2024-01-03 14:52:12 +0530

    Add files via upload

---
+++ SQL/String Functions/Other String Functions.sql
@@ -0,0 +1,9 @@
+SELECT INSERT('Hello Bobby', 6, 0, 'There');  -- HelloThere Bobby
+
+SELECT LEFT('omghahalol!', 3); -- omg
+
+SELECT RIGHT('omghahalol!', 4); -- lol!
+
+SELECT REPEAT('ha', 4); -- hahahaha
+
+SELECT TRIM('  pickle  '); -- pickle
