944c7d84c8472026d361db009cfa909aa9857347
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2024-01-04 10:44:07 +0530

    Update Limit.sql

--- SQL/Refining Selections/Limit.sql
+++ SQL/Refining Selections/Limit.sql
@@ -1,7 +1,7 @@
 
 SELECT book_id, title, released_year from books limit 5; -- It'll give first 5
 
--- Print first 5 released books
+-- Print the first 5 released books
 select title,released_year from books order by released_year limit 5;
 
 -- Sort books by released year and print books between 3 and 10;
