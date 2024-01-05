31c8432d439c2436bb15102d84def6ce5a42e73b
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2024-01-05 14:34:26 +0530

    Update Time Function.sql

--- SQL/Advanced Data Types (Revisit)/Time Function.sql
+++ SQL/Advanced Data Types (Revisit)/Time Function.sql
@@ -1,3 +1,17 @@
 select name, birthtime, hour(birthtime) from people;
 
 -- In the same we can find the minute & second 
+
+SELECT 
+    birthtime,
+    HOUR(birthtime),
+    MINUTE(birthtime)
+FROM people;
+ 
+SELECT 
+    birthdt,
+    MONTH(birthdt),
+    DAY(birthdt),
+    HOUR(birthdt),
+    MINUTE(birthdt)
+FROM people;
