7a60e9a8a7430fa3b6c3817bdff7d156348e7758
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2024-01-06 16:15:58 +0530

    Update Logical AND.sql

--- SQL/Comparison & Logical Operators/Logical AND.sql
+++ SQL/Comparison & Logical Operators/Logical AND.sql
@@ -1,4 +1,6 @@
--- SELECT books written by Dave Eggers,published after the year 2010
+-- Both side of a AND should be true
+
+-- SELECT books written by Dave Eggers, published after the year 2010
 
 select title, author_fname, author_lname, released_year from books where 
 author_fname = 'Dave' and author_lname = 'Eggers' and released_year > 2010;
