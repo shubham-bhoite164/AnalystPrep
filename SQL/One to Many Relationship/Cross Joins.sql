1b2fe76416d76ffc7ef4ae44e659d7f3ae8e01cd
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2024-01-10 13:49:03 +0530

    Update Cross Joins.sql

--- SQL/One to Many Relationship/Cross Joins.sql
+++ SQL/One to Many Relationship/Cross Joins.sql
@@ -5,6 +5,10 @@ where last_name = 'George';
 
 select * from orders where customer_id = 1;
 
--- Making it in ine query
+-- Making it in the query
 SELECT * FROM orders 
 WHERE customer_id = (SELECT id FROM customers WHERE last_name = 'George');
+
+
+-- To perform a (kind of useless) cross-join:
+SELECT * FROM customers, orders;
