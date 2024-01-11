2ab84afcdd6dac07ae02bdf86f96188c13f03ca0
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2024-01-11 14:34:16 +0530

    Add files via upload

---
+++ SQL/Many to Many Relationship/TV Challenge #3.sql
@@ -0,0 +1,31 @@
+-- Make below thing happen
+
+-- +------------+-----------+--------+
+-- | first_name | last_name | rating |
+-- +------------+-----------+--------+
+-- | Thomas     | Stoneman  |    8.0 |
+-- | Thomas     | Stoneman  |    8.1 |
+-- | Thomas     | Stoneman  |    7.0 |
+-- | Thomas     | Stoneman  |    7.5 |
+-- | Thomas     | Stoneman  |    9.5 |
+-- | Wyatt      | Skaggs    |    7.5 |
+-- | Wyatt      | Skaggs    |    7.6 |
+-- | Wyatt      | Skaggs    |    9.3 |
+-- | Wyatt      | Skaggs    |    6.5 |
+-- | Wyatt      | Skaggs    |    8.4 |
+-- | Wyatt      | Skaggs    |    9.1 |
+-- | Wyatt      | Skaggs    |    7.8 |
+-- | Wyatt      | Skaggs    |    5.5 |
+-- | Wyatt      | Skaggs    |    8.5 |
+-- | Kimbra     | Masters   |    8.5 |
+-- | Kimbra     | Masters   |    8.0 |
+-- | Kimbra     | Masters   |    7.1 |
+-- | Kimbra     | Masters   |    7.8 |
+-- | Kimbra     | Masters   |    9.0 |
+-- | Kimbra     | Masters   |    7.8 |
+-- +------------+-----------+--------+
+
+
+select first_name, last_name, rating 
+from reviewers
+join reviews on reviewers.id = reviews.reviewer_id;
