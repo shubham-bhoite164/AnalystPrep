3b7661618f2975f78fc6e242b92ae622a95dc02d
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2024-02-02 11:55:30 +0530

    Update Sum.sql

--- SQL/Aggregate Func/Sum.sql
+++ SQL/Aggregate Func/Sum.sql
@@ -2,7 +2,7 @@
 select sum(pages) from books;
 
 
--- Sum all the pages each author has written 
+-- Sum all the pages each author has written.
 select author_fname, author_lname, sum(pages)
 from books 
 group by author_fname,author_lname;
